c851d3b960057ab82751b7635036605e50a3c511
Harsh Sinha <107769209+s-harsh19@users.noreply.github.com>
2022-09-03 21:49:45 +0530

    Update bubble_sort.js

--- scripts/bubble_sort.js
+++ scripts/bubble_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+
     
     *****************
 */
@@ -46,7 +46,7 @@ function Bubble()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+   
     
     *****************
 */
